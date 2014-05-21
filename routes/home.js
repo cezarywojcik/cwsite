9337a99caa58e99edda04d5a43e84103082033bd
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-05-20 21:37:13 -0700

    updated styling and views

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,13 @@
  */
 
 exports.home = function(req, res) {
-  res.render("home");
+  res.render("home", {
+    title: "Home",
+    article: {
+      title: "Test Title",
+      timecreated: "May 20, 2014",
+      content: "test content",
+      nav: "nav stuff"
+    }
+  });
 };
